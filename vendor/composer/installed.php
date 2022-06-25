53b0cbbcaa4ed81d30d118a7e15200078207ac75
Stephen  Adehor <stephen.adehor@CNL-Admin-2.local>
2022-06-25 02:27:33 +0100

    add vendor folder for autoloading

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'stephen.adehor/php-tutorial',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7dea9c33d9749e5e5b7473849cdc147cb576ae72',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'stephen.adehor/php-tutorial' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7dea9c33d9749e5e5b7473849cdc147cb576ae72',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
